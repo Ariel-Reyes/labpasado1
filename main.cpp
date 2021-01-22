fa8c9c23272dc865e3e87a00982d84c44b413e43
Ariel-Reyes <73078760+Ariel-Reyes@users.noreply.github.com>
2021-01-22 13:47:49 -0600

    AGREGACION DEL MENU

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<math.h>
+using namespace std; 
+
+int main() {
+	
+	
+int op; 
+while(op!=3){
+	cout<<"--------Menu---------"<<endl;
+	cout<<"1) SUMATORIA"<<endl; 
+	cout<<"2) FORMULA"<<endl;
+	cout<<"3) SALIR"<<endl;
+	cout<< "INGRESE LA OPCION QUE DESEA:"<<endl;
+	cin>>op; 
+	
+	switch(op){
+		case 1:
+			
+			
+			
+			break;
+			
+			case 2:
+				
+				
+			break;
+			default:
+				cout<<"...."<<endl;
+			break;   
+	}
+	
+	
+	
+	
+}
+	
+
+	
+	
+	
+	
+	return 0;
+}
